eafc0c497cc3e87cf742d78b02eae93e1c5ce699
cleytonchagasbr <cleyton-chagas@hotmail.com>
2021-02-15 22:47:44 -0300

    criando o projeto e configuracoes iniciais

---
+++ todo/lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Todo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Lista de tarefas"),
+      ),
+      body: Container(
+        child: Center(child: Text("Oláaaaa"))
+      )
+    );
+  }
+}
+
+
+
+
+
+
+
+
