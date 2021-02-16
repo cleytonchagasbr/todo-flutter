7afe6104803104453e657c2852df7d485131167f
cleytonchagasbr <cleyton-chagas@hotmail.com>
2021-02-15 23:20:44 -0300

    adicionando list view

--- todo/lib/main.dart
+++ todo/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'models/item.dart';
+
 void main() {
   runApp(App());
 }
@@ -19,19 +21,46 @@ class App extends StatelessWidget {
 }
 
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  var items = new List<Item>();
+
+  HomePage() {
+    items = [];
+    items.add(Item(title: "Teste", done: false));
+    items.add(Item(title: "Teste", done: false));
+    items.add(Item(title: "Teste", done: false));
+  }
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Lista de tarefas"),
       ),
-      body: Container(
-        child: Center(child: Text("Oláaaaa"))
+      body: ListView.builder(
+        itemCount: widget.items.length,
+        itemBuilder: (BuildContext ctxt, int index){
+          final item = widget.items[index];
+
+          return CheckboxListTile(
+            title: Text(item.title),
+            key: Key(item.title),
+            value: item.done,
+            onChanged: (value) {
+
+            },
+          );
+        },
       )
     );
   }
 }
+
 
 
 
